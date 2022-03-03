e84d2dd211af1431147ec4398b200d23c8c7f41f
Tom Kraak <tom.kraak@simplepractice.com>
2022-03-03 10:15:37 -0500

    skip verification of authenticity token

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,4 @@
 class FriendsController < JSONAPI::ResourceController
+  # https://dev.to/ben/actioncontroller-invalidauthenticitytoken-what-s-going-on-here-2828
+  skip_before_action :verify_authenticity_token
 end
